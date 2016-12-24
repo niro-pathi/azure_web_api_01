42fdc5b11a155f5847c3e5ee39dce6c19a680629
Niroshana Pathirana <niroshana.pathirana@outlook.com>
2016-12-25 01:49:01 +1100

    Cosmetic changes added

--- app.js
+++ app.js
@@ -3,6 +3,7 @@
 const hapi = require('hapi');
 const server = new hapi.Server();
 var routes = require('./routes/routes.js'); //require routes
+
 server.connection({host:'localhost', port: 3000});
 
 server.route(routes); //add routes
